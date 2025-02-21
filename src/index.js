ecb247357d233f7e7cbb2514081c417c625aa9f5
Benne420 <benedikt.blazeowsky@420pharma.eu>
2025-02-21 14:27:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
